57d1e833c6bb0559366c6045c51589d4b4ec8baf
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2022-12-31 18:43:36 +0530

    ADD youtube container

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Navbar from "./components/navbar/Navbar";
+import Player from "./components/player/Player";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
       <Navbar />
+      <Player />
     </div>
   );
 }
---
+++ src/components/player/Player.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./player.css";
+
+export default function Player() {
+  return <div className="player"></div>;
+}
