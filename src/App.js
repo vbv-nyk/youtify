c4ff849559907619a5dfd1133b53e29de6b01974
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-01 02:17:12 +0530

    Remove duplicate albums loading

--- src/App.js
+++ src/App.js
@@ -5,39 +5,47 @@ import Navbar from "./components/navbar/Navbar";
 import Player from "./components/player/Player";
 import PlayerControl from "./components/controls/PlayerControl";
 import Albums from "./components/album-card/Albums";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-// const api_key = "AIzaSyCALUbMM1nnBb7wP31KnRHog9hb7Bgkx00";
-// const channel_id = "UC-9-kyTW8ZkZNDHQJ6FgpwQ";
-// async function fetchData() {
-//   let fetched = 0;
-//   let maxResults = 100;
-//   let nextPageToken = "";
-//   while (fetched < maxResults) {
-//     const youtubeData = await fetch(
-//       `https://www.googleapis.com/youtube/v3/playlists?channelId=${channel_id}&part=snippet&key=${api_key}&maxResults=50&pageToken=${nextPageToken}`
-//     );
-//     const youtubeDataJSON = await youtubeData.json();
-//     maxResults = youtubeDataJSON.pageInfo.totalResults;
-//     for (let item of youtubeDataJSON.items) {
-//       if (
-//         item.snippet.title.charAt(0) == "R" &&
-//         item.snippet.title.charAt(1) == "D"
-//       ) {
-//         console.log("True");
-//       } else {
-//         console.log(item.snippet.title);
-//       }
-//     }
-//     fetched += 50;
-//     nextPageToken = youtubeDataJSON.nextPageToken;
-//   }
-// }
-
-// fetchData().then((result) => {});
+const api_key = "AIzaSyCALUbMM1nnBb7wP31KnRHog9hb7Bgkx00";
+const channel_id = "UC-9-kyTW8ZkZNDHQJ6FgpwQ";
+const albumData = [];
+async function fetchData() {
+  let fetched = 0;
+  let maxResults = 100;
+  let nextPageToken = "";
+  let tempAlbumData = [];
+  while (fetched < maxResults) {
+    const youtubeData = await fetch(
+      `https://www.googleapis.com/youtube/v3/playlists?channelId=${channel_id}&part=snippet&key=${api_key}&maxResults=50&pageToken=${nextPageToken}`
+    );
+    const youtubeDataJSON = await youtubeData.json();
+    maxResults = youtubeDataJSON.pageInfo.totalResults;
+    for (let item of youtubeDataJSON.items) {
+      tempAlbumData.push(item);
+    }
+    fetched += 50;
+    nextPageToken = youtubeDataJSON.nextPageToken;
+  }
+  let unique = [];
+  tempAlbumData.forEach((element) => {
+    if (!unique.includes(element.snippet.title)) {
+      unique.push(element.snippet.title);
+      albumData.push(element);
+    }
+  });
+  console.log(albumData);
+}
 
 function App() {
   const [searchEnabled, setSearchEnabled] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    fetchData().then(() => {
+      setLoaded(true);
+      console.log(loaded);
+    }, []);
+  });
   return (
     <div className="App">
       <Sidebar
@@ -45,9 +53,7 @@ function App() {
         setSearchEnabled={setSearchEnabled}
       />
       <Navbar searchEnabled={searchEnabled} />
-      <Player>
-        <Albums></Albums>
-      </Player>
+      <Player>{loaded && <Albums albumData={albumData} />}</Player>
       <PlayerControl />
     </div>
   );
--- src/components/album-card/Albums.jsx
+++ src/components/album-card/Albums.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { Children } from "react";
+import "./albums.css";
 
-function AlbumCard({ props }) {
+function AlbumCard(props) {
+  console.log(props.album.snippet);
   return (
-    <div className="album-item">
-      <img src={props.src} alt="" srcset="" />
-      <div className={props.title}></div>
-      <div className={props.desc}></div>
+    <div className="album-item" id="props.album.id">
+      <img
+        src={props.album.snippet.thumbnails.medium.url}
+        alt={props.album.snippet.thumbnails.default.url}
+        loading="lazy"
+      />
+      <div className="album-name">{props.album.snippet.title}</div>
+      <div className="album-description">{props.album.snippet.description}</div>
     </div>
   );
 }
 
-function AlbumCards({ data }) {
-  return <></>;
+function AlbumCards(props) {
+  const cards = props.albumData.map((card) => {
+    return <AlbumCard album={card} />;
+  });
+  return <div>{cards}</div>;
 }
 
-export default function Albums() {
-  return <AlbumCards />;
+export default function Albums(props) {
+  return <AlbumCards albumData={props.albumData} />;
 }
