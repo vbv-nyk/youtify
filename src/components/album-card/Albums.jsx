379a2efc66e39c6483aa653f4ba70c4b316a264b
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-01-01 12:32:33 +0530

    REVAMP album cards design

--- src/components/album-card/Albums.jsx
+++ src/components/album-card/Albums.jsx
@@ -4,36 +4,45 @@ import "./albums.css";
 function AlbumCard(props) {
   return (
     <div className="album-item">
-      {/* <img
-        src={props.album.snippet.thumbnails.medium.url}
+      <img
+        src={props.album.snippet.thumbnails.high.url}
         alt={props.album.snippet.thumbnails.default.url}
         loading="lazy"
       />
-      <div className="album-name">{props.album.snippet.title}</div> */}
+      {/* <div className="album-name">{props.album.snippet.title}</div> */}
     </div>
   );
 }
 
+function AlbumController({ albumCount, setAlbumCount, albumData }) {
+  return (
+    <div className="navigation">
+      <button
+        className="back-button"
+        onClick={() => setAlbumCount((n) => Math.max(n - 9, 0))}
+      >{`Previous Page`}</button>
+      <button
+        className="forward-button"
+        onClick={() =>
+          setAlbumCount((n) => Math.min(n + 9, albumData.length - 9))
+        }
+      >{`Next Page`}</button>
+    </div>
+  );
+}
 function AlbumCards(props) {
   const [albumCount, setAlbumCount] = useState(0);
-  const currentAlbumData = props.albumData.slice(albumCount, albumCount + 6);
+  const currentAlbumData = props.albumData.slice(albumCount, albumCount + 9);
   const cards = currentAlbumData.map((card) => {
     return <AlbumCard album={card} key={card.id} />;
   });
   return (
     <div className="album-controller">
-      <div className="navigation">
-        <button
-          className="back-button"
-          onClick={() => setAlbumCount((n) => Math.max(n - 6, 0))}
-        >{`Next Page`}</button>
-        <button
-          className="forward-button"
-          onClick={() =>
-            setAlbumCount((n) => Math.min(n + 6, props.albumData.length - 6))
-          }
-        >{`Previous Page`}</button>
-      </div>
+      <AlbumController
+        albumCount={albumCount}
+        setAlbumCount={setAlbumCount}
+        albumData={props.albumData}
+      />
       <div className="album-container">{cards}</div>;
     </div>
   );
